4dabc1d0e446356a582fa8b5d91278cadec7727b
phanit1 <babithota5@msitprogram.net>
2019-09-24 18:46:34 +0530

    Day1 Day2 Done

---
+++ Module 1/MyName/MyName.java
@@ -0,0 +1,10 @@
+public class MyName
+{
+    public static void main(String args[])
+    {
+        System.out.println("Phaneendhar");
+    }
+}
+
+// git config --global user.email "babithota5@msitprogram.net"
+// git config --global user.name "phanit1"
