e5fe65b2c982f766446e8d070140b7f10dfb8530
phanit1 <babithota5@msitprogram.net>
2019-09-24 22:43:12 +0530

    Day3

---
+++ Module3/Stats/Stats.java
@@ -0,0 +1,109 @@
+/**
+ * Given an array of integers Find the mean, median, mode, standard deviation
+ * and variance.
+ * 
+ * Mean
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Median
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Mode
+ * Input : [1,1,2,2,3]
+ * Output : [1,2]
+ * 
+ * Variance
+ * Input : [1,2,3,4,5]
+ * Output : 2.0
+ * 
+ * Standard Deviation
+ * Input : [1,2,3,4,5]
+ * Output : 1.41421356
+ * 
+ * @author Siva Sankar
+ */
+
+import java.util.*;
+
+import javax.jws.WebParam.Mode;
+
+public class Stats {
+
+    /**
+     * This method should return the mean of the elements of the array.
+     * If the arr is empty (length is 0), return the mean as 0.
+     * @param arr, the input array contians elements.
+     * @return the mean of the array.
+     */
+    public static double mean(int[] arr) {
+        //  Your code goes here....
+        int size=arr.length;
+        double sum=0;
+        for ( int i=0;i<size;i++)
+        {
+            sum+=arr[i];    
+        }
+        double mean=sum/size;
+        return mean;
+    }
+
+    /**
+     * This method returns the median of the elements of the array.
+     * Prerequisite : The array should be sorted to find the median.
+     * 
+     * Hint : Arrays.sort(arr) sorts the elements of the array.
+     * 
+     * @param arr, the input array contains random elements (not in sorted order)
+     * @return the median of the elements of the array.
+     */
+    public static double median(int[] arr) {
+        //  Your code goes here...
+        int[] sortArr=Arrays.sort(arr);
+        int a=sortArr.length;
+        if(a%2!=0)
+            median=sortArr[a/2];
+        else
+            median=(sortArr[a/2]+sortArr[(a+1)/2])/2;
+        return median;
+    }
+
+    /**
+     * This method returns the mode of the array. If there is no mode, then it returns
+     * 0 as the mode.
+     * 
+     * @param arr, the input array.
+     * @return the mode of the array, 0 otherwise if there is no mode.
+     */
+    public static int mode(int[] arr) {
+        //  Your code goes here....
+
+        return Mode(arr);
+    }
+
+    /**
+     * This method returns the variance of the input array.
+     * Make use of the mean functon which has already written.
+     * 
+     * @param arr, the input array
+     * @return the variance of the array.
+     */
+    public static double variance(int[] arr) {
+        //  Your code goes here....
+
+        return 0.0;
+    }
+
+    /**
+     * This method returns the standard deviation of the array.
+     * Hint : You can find the square root using Math.sqrt() method.
+     * @param arr, the input array
+     * @return the standard deviation of the input array.
+     */
+    public static double standardDeviation(int[] arr) {
+        //  Your code goes here....
+
+        return 0.0;
+    }
+}
